c4abd47e7e5c7566e3274e30fa88e6e6be5465e0
Yafei Guo <yafei.guo@ygomi.com>
2020-01-14 12:46:27 +0800

    update dict name

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # start_python
-learn python
+## Data type
+```
+number string list tuple set dict
+可变数据类型： list dict set
+不可变数据类型： number string tuple
+```
---
+++ records/feach_record.py
@@ -0,0 +1,51 @@
+import json
+import time
+import requests
+
+
+def re_clac_rainy_snow_records(ip: str, port: str, env: str, airflow_ip: str, airflow_port: str, airflow_env: str):
+    # get record list
+    url = 'http://' + ip + ':' + port + '/' + env + ':record/record_info/_search'
+    headers = {
+        "content-type": "application/json"
+    }
+    data = {
+        "query" : {
+            "nested" : {
+                "path" : "file_attr",
+                "query" : {
+                    "bool" : {
+                        "should" :[
+                            {"term" : {"file_attr.weather.name" : "Rainy"}},
+                            {"term" : {"file_attr.weather.name" : "Snow"}}
+                        ]
+                    }
+                }
+            }
+        },
+        "_source" :["record_code"],
+        "size":10000
+    }
+    response = requests.post(url=url, headers=headers, data=json.dumps(data)).json()
+    record_list = []
+    for obj in response['hits']['hits']:
+        # trigger dag
+        airflow_url = "http://" + airflow_ip + ":" + airflow_port + \
+                      '/api/experimental/dags/' + airflow_env + '_record_oea_dag/dag_runs'
+        dag_run_data = {
+            "run_id": obj['_id'] + "_reclac_weather",
+            "conf": {
+                "recordCode": obj['_id']
+            }
+        }
+        airflow_headers = {
+            "content-type": "application/json"
+        }
+        response = requests.post(url=airflow_url, headers=airflow_headers, data=json.dumps(dag_run_data))
+        time.sleep(1.5)
+        print(response)
+        record_list.append(obj['_id'])
+    return record_list
+
+
+# print(re_clac_rainy_snow_records('10.69.130.183', '9200', 'int', '10.69.144.145', '8080', 'int'))
