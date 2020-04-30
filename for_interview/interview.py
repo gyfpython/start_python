87024a77c53334d76e571d2b7ec4cbe7afd26706
gyfpython <1453365491@qq.com>
2020-04-30 22:23:33 +0800

    get all sorted of a string

---
+++ for_interview/interview.py
@@ -0,0 +1,23 @@
+def all_str_sort(string: str):
+    if not string:
+        return None
+    length = len(string)
+    origin_list = [string[0]]
+    for i in range(1, length):
+        origin_list = insert_test(string[i], origin_list)
+    print(len(origin_list), origin_list)
+
+
+def insert_test(string1: str, test_list: list):
+    temp_list = []
+    for i in range(len(test_list)):
+        for j in range(len(test_list[i])+1):
+            str_list = list(test_list[i])
+            str_list.insert(j, string1)
+            result_str = ''.join(str_list)
+            temp_list.append(result_str)
+    return temp_list
+
+
+test = 'qwert'
+all_str_sort(test)
